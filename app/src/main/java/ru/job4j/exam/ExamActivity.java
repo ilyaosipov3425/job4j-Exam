9af27945134862a2f235763d1f40bcf8b2820054
Ilya Osipov <bullet3425@yandex.ru>
2019-03-25 13:32:42 +0300

    Создание проекта и запуск через эмулятор Genymotion.[#61514]

---
+++ app/src/main/java/ru/job4j/exam/ExamActivity.java
@@ -0,0 +1,13 @@
+package ru.job4j.exam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam);
+    }
+}
